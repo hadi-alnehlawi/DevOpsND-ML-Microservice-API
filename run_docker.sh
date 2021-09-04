9708625f1d3c8c89b0bd05be720b585ce2d285df
hadi <hadi.alnehlawi@gmail.com>
2021-09-04 11:40:48 -0300

    update sh docker images

--- run_docker.sh
+++ run_docker.sh
@@ -4,13 +4,20 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag devops-ml-api .
+export IMAGENAME="devops-ml-api"
+if [[ "$(docker images -q devops-ml-api 2> /dev/null)" == "" ]]; then
+  printf "\n>>>>>>>>>>>\n";
+  printf "Building Images";
+  printf "\n>>>>>>>>>>>\n";
+  docker build --tag devops-ml-api .;
+fi
 
-# Step 2: 
-# List docker images
-export IMAGEID=$(docker image ls devops-ml-api --format "{{.ID}}")
+
+# # Step 2: 
+# # List docker images
+# export IMAGEID=$(docker image ls devops-ml-api --format "{{.ID}}")
 
 
-# Step 3: 
-# Run flask app
- docker run $IMAGEID
+# # Step 3: 
+# # Run flask app
+#  docker run $IMAGEID
