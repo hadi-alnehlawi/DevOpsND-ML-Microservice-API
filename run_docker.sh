7a90f9256d2caf4e087b6c8f5a4a21342d688344
hadi <hadi.alnehlawi@gmail.com>
2021-09-04 12:10:48 -0300

    remove container after running

--- run_docker.sh
+++ run_docker.sh
@@ -5,16 +5,12 @@
 # Step 1:
 # Build image and add a descriptive tag
 export IMAGENAME="devops-ml-api"
-if [[ "$(docker images -q {IMAGENAME} 2> /dev/null)" == "" ]]; then
-  printf "\n>>>>>>>>>>>\n"
-  printf "Building Images"
-  printf "\n>>>>>>>>>>>\n\n"
-  docker build --tag devops-ml-api .
-fi
-# # Step 2: 
-# # List docker images
+docker build --tag $IMAGENAME .
+
+# Step 2: 
+# List docker images
 export IMAGEID=$(docker images -q $IMAGENAME)
-printf $IMAGEID
-# # Step 3: 
-# # Run flask app
- docker run $IMAGEID
+
+# Step 3: 
+# Run flask app
+docker run --rm $IMAGEID
