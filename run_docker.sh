f74a300961d738505c158bf71ad47cf2789959f1
hadi <hadi.alnehlawi@gmail.com>
2021-09-07 11:28:41 -0300

    tag docker images

--- run_docker.sh
+++ run_docker.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
-
+# IMAGE=devops-ml-api
 # Step 1:
 # Build image and add a descriptive tag
-export IMAGENAME="devops-ml-api"
-if [[ "$(docker images -q ${IMAGENAME} 2> /dev/null)" == "" ]]; then
+export IMAGE=$1
+if [[ "$(docker images -q ${IMAGE} 2> /dev/null)" == "" ]]; then
   printf "\n\nIt is not existed\n\n"
-  docker build --tag $IMAGENAME .
+  docker build --tag $IMAGE .
 fi
 
 # Step 2: 
 # List docker images
-export IMAGEID=$(docker images -q $IMAGENAME)
+export IMAGEID=$(docker images -q $IMAGE)
 
 # Step 3: 
 # Run flask app
--- upload_docker.sh
+++ upload_docker.sh
@@ -2,14 +2,17 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `run_docker.sh`
-
+# IMAGE=devops-ml-api
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+export DOCKERID="hadialnehlawi"
+export IMAGE=$1
+export dockerpath=$DOCKERID/$IMAGE
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag $IMAGE $dockerpath
 
 # Step 3:
 # Push image to a docker repository
